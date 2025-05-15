671390ac6ee1f99e0c91c59cb066322075b3e72b
Zander Work <zander@zanderwork.com>
2025-05-14 22:18:17 -0400

    initial vga screen buffer impl from the blog works, time to v2 it

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,17 @@
 
 use core::panic::PanicInfo;
 
+mod vga;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
+/// Entry point
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
